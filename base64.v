47a2783a90546eafd9a76b02685289ee7f9963ad
Igor Aleksanov <popzxc@yandex.ru>
2019-10-19 09:20:01 +0300

    Add decode function

--- base64.v
+++ base64.v
@@ -6,38 +6,118 @@ module base64
 
 const (
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    m1 = 63 << 18
-    m2 = 63 << 12
-    m3 = 63 << 6
+    max_idx = alphabet.len - 1
+    m1 = max_idx << 18
+    m2 = max_idx << 12
+    m3 = max_idx << 6
 )
 
-pub fn base64(v []byte) string {
+// Encodes a provided bytes sequence to the base64 string.
+pub fn base64_encode(input []byte) string {
     mut result := ""
+    mut current := 0
+    mut left := input.len
     mut d := 0
-    mut a := 0
-    mut l := v.len
 
-    for (l > 2) {
-	    d = int(v[a] << 16) | int(v[a + 1] << 8) | int(v[a + 2])
-	    result += alphabet[( d & m1 ) >> 18 ].str()
-	    result += alphabet[( d & m2 ) >> 12 ].str()
-	    result += alphabet[( d & m3 ) >>  6 ].str()
-	    result += alphabet[d & 63 ].str()
-        a += 3
-	    l -= 3
-	}
+    for (left > 2) {
+        d = int(input[current] << 16) | int(input[current + 1] << 8) | int(input[current + 2])
+        result += alphabet[( d & m1 ) >> 18 ].str()
+        result += alphabet[( d & m2 ) >> 12 ].str()
+        result += alphabet[( d & m3 ) >>  6 ].str()
+        result += alphabet[d & max_idx ].str()
+        current += 3
+        left -= 3
+    }
 
-	if (l == 2) {
-	    d = int(v[a] << 16) | int(v[a + 1] << 8)
-	    result += alphabet[( d & m1 ) >> 18].str()
-	    result += alphabet[( d & m2 ) >> 12].str()
-	    result += alphabet[( d & m3 ) >>  6].str()
-	    result += '='
-	} else if (l == 1) {
-	    d = int(v[a] << 16)
-	    result += alphabet[( d & m1 ) >> 18].str()
-	    result += alphabet[( d & m2 ) >> 12].str()
-	    result += "=="
-	}
+    if (left == 2) {
+        d = int(input[current] << 16) | int(input[current + 1] << 8)
+        result += alphabet[( d & m1 ) >> 18].str()
+        result += alphabet[( d & m2 ) >> 12].str()
+        result += alphabet[( d & m3 ) >>  6].str()
+        result += '='
+    } else if (left == 1) {
+        d = int(input[current] << 16)
+        result += alphabet[( d & m1 ) >> 18].str()
+        result += alphabet[( d & m2 ) >> 12].str()
+        result += "=="
+    }
     return result
+}
+
+fn find_index(val byte) ?int {
+    if (`A` <= val && val <= `Z`) {
+        return int(val - `A`)
+    }
+    if (`a` <= val && val <= `z`) {
+        return int(val - `a` + 26)
+    }
+    if (`0` <= val && val <= `9`) {
+        return int(val - `0` + 52)
+    }
+    if (val == `+`) {
+        return 62
+    }
+    if (val == `/`) {
+        return 63
+    }
+    return error("Incorrect byte $val")
+}
+
+// Decodes a provided base64 string to the bytes sequence.
+pub fn base64_decode(input string) ?[]byte {
+    mut current := 0
+    mut acc := 0
+    mut output := []byte
+
+    if (input.len % 4 != 0) {
+        return error("Input string is not aligned")
+    }
+
+    for (current != input.len) {
+        b1 := input[current]
+        b2 := input[current + 1]
+        b3 := input[current + 2]
+        b4 := input[current + 3]
+        current += 4
+
+        i1 := find_index(b1) or {
+            return error(err)
+        }
+        i2 := find_index(b2) or {
+            return error(err)
+        }
+
+        acc = i1 << 2
+        acc |= i2 >> 4
+
+        output << byte(acc)
+
+        if b3 == `=` {
+            break
+        }
+
+        i3 := find_index(b3) or {
+            return error(err)
+        }
+
+        acc = (i2 & 0xF) << 4
+        acc |= i3 >> 2
+
+        output << byte(acc)
+
+        if b4 == `=` {
+            break
+        }
+
+        i4 := find_index(b4) or {
+            return error(err)
+        }
+
+        acc = (i3 & 0x3) << 6
+        acc |= i4
+
+        output << byte(acc)
+    }
+
+    return output
 }
--- base64_test.v
+++ base64_test.v
@@ -1,6 +1,96 @@
 import base64
 
-fn test_call() {
-    test := [byte(100), 200, 300]
-    res := base64.base64(test)
+struct TestCase {
+    input string
+    expected string
+}
+
+fn string_to_bytes(data string) []byte {
+     mut buf := []byte
+
+    for chr in data {
+        buf << chr
+    }
+
+    return buf
+}
+
+fn test_encode() {
+    test_vector := [
+        TestCase {
+            input: ''
+            expected: ''
+        },
+        TestCase {
+            input: 'a'
+            expected: 'YQ=='
+        },
+        TestCase {
+            input: 'abcdefGHIjKLMno'
+            expected: 'YWJjZGVmR0hJaktMTW5v'
+        },
+        TestCase {
+            input: ' jkahsd jfhjfasd hfjahd jsakh jfdash ljfhdsj lhfdjash jfklasdhl jfhdsal hfdjskalh jflkdshj klfhdsla hfjdshaj lfh asjldhf jalhdsjk hfjdasl hjfkdlhasjk hjflkdsah jklfahsdjl hfjakdlshj lhjaslkdhjfk lhasdjlhf jlkasdhjfkl hajsdlhf jkaldshj kfhladsh jkfahdsjkl hfjqhefuoihjxzhxkvcbnjvgwbruiohfgisdjklvcnkxbgvlwhreuifwhdjzlvnb jblriheifuohfiujwflksdabjkvhuwieohflqhwefknbczknjxvbiurooheqfuihejvcbad jnkvbroqhuifh jdklfjb sjdvb u4i2127 8rhe9ph fg8h434jk2n fjkfneg0 923h4thn43ljtn 234890 ht89dhklsv nfjng2pg3894ht uq2uin jefnid0 hv8792htfl 2nj 43892r fisldf ug23y3r0 hgrjl nzsfdc lmvn2983h 32h;qjrka1234u879hrgjrle gl 2jhiht43 2 32ji 1hjuo342h uth3l hjkfsdhgl'
+            expected: 'IGprYWhzZCBqZmhqZmFzZCBoZmphaGQganNha2ggamZkYXNoIGxqZmhkc2ogbGhmZGphc2ggamZrbGFzZGhsIGpmaGRzYWwgaGZkanNrYWxoIGpmbGtkc2hqIGtsZmhkc2xhIGhmamRzaGFqIGxmaCBhc2psZGhmIGphbGhkc2prIGhmamRhc2wgaGpma2RsaGFzamsgaGpmbGtkc2FoIGprbGZhaHNkamwgaGZqYWtkbHNoaiBsaGphc2xrZGhqZmsgbGhhc2RqbGhmIGpsa2FzZGhqZmtsIGhhanNkbGhmIGprYWxkc2hqIGtmaGxhZHNoIGprZmFoZHNqa2wgaGZqcWhlZnVvaWhqeHpoeGt2Y2JuanZnd2JydWlvaGZnaXNkamtsdmNua3hiZ3Zsd2hyZXVpZndoZGp6bHZuYiBqYmxyaWhlaWZ1b2hmaXVqd2Zsa3NkYWJqa3ZodXdpZW9oZmxxaHdlZmtuYmN6a25qeHZiaXVyb29oZXFmdWloZWp2Y2JhZCBqbmt2YnJvcWh1aWZoIGpka2xmamIgc2pkdmIgdTRpMjEyNyA4cmhlOXBoIGZnOGg0MzRqazJuIGZqa2ZuZWcwIDkyM2g0dGhuNDNsanRuIDIzNDg5MCBodDg5ZGhrbHN2IG5mam5nMnBnMzg5NGh0IHVxMnVpbiBqZWZuaWQwIGh2ODc5Mmh0ZmwgMm5qIDQzODkyciBmaXNsZGYgdWcyM3kzcjAgaGdyamwgbnpzZmRjIGxtdm4yOTgzaCAzMmg7cWpya2ExMjM0dTg3OWhyZ2pybGUgZ2wgMmpoaWh0NDMgMiAzMmppIDFoanVvMzQyaCB1dGgzbCBoamtmc2RoZ2w='
+        }
+    ]
+
+    for test in test_vector {
+        res := base64.base64_encode(string_to_bytes(test.input))
+        assert res == test.expected
+    }
+}
+
+fn test_decode() {
+    test_vector := [
+        TestCase {
+            input: ''
+            expected: ''
+        },
+        TestCase {
+            input: 'YQ=='
+            expected: 'a'
+        },
+        TestCase {
+            input: 'YWJjZGVmR0hJaktMTW5v'
+            expected: 'abcdefGHIjKLMno'
+        },
+        TestCase {
+            input: 'IGprYWhzZCBqZmhqZmFzZCBoZmphaGQganNha2ggamZkYXNoIGxqZmhkc2ogbGhmZGphc2ggamZrbGFzZGhsIGpmaGRzYWwgaGZkanNrYWxoIGpmbGtkc2hqIGtsZmhkc2xhIGhmamRzaGFqIGxmaCBhc2psZGhmIGphbGhkc2prIGhmamRhc2wgaGpma2RsaGFzamsgaGpmbGtkc2FoIGprbGZhaHNkamwgaGZqYWtkbHNoaiBsaGphc2xrZGhqZmsgbGhhc2RqbGhmIGpsa2FzZGhqZmtsIGhhanNkbGhmIGprYWxkc2hqIGtmaGxhZHNoIGprZmFoZHNqa2wgaGZqcWhlZnVvaWhqeHpoeGt2Y2JuanZnd2JydWlvaGZnaXNkamtsdmNua3hiZ3Zsd2hyZXVpZndoZGp6bHZuYiBqYmxyaWhlaWZ1b2hmaXVqd2Zsa3NkYWJqa3ZodXdpZW9oZmxxaHdlZmtuYmN6a25qeHZiaXVyb29oZXFmdWloZWp2Y2JhZCBqbmt2YnJvcWh1aWZoIGpka2xmamIgc2pkdmIgdTRpMjEyNyA4cmhlOXBoIGZnOGg0MzRqazJuIGZqa2ZuZWcwIDkyM2g0dGhuNDNsanRuIDIzNDg5MCBodDg5ZGhrbHN2IG5mam5nMnBnMzg5NGh0IHVxMnVpbiBqZWZuaWQwIGh2ODc5Mmh0ZmwgMm5qIDQzODkyciBmaXNsZGYgdWcyM3kzcjAgaGdyamwgbnpzZmRjIGxtdm4yOTgzaCAzMmg7cWpya2ExMjM0dTg3OWhyZ2pybGUgZ2wgMmpoaWh0NDMgMiAzMmppIDFoanVvMzQyaCB1dGgzbCBoamtmc2RoZ2w='
+            expected: ' jkahsd jfhjfasd hfjahd jsakh jfdash ljfhdsj lhfdjash jfklasdhl jfhdsal hfdjskalh jflkdshj klfhdsla hfjdshaj lfh asjldhf jalhdsjk hfjdasl hjfkdlhasjk hjflkdsah jklfahsdjl hfjakdlshj lhjaslkdhjfk lhasdjlhf jlkasdhjfkl hajsdlhf jkaldshj kfhladsh jkfahdsjkl hfjqhefuoihjxzhxkvcbnjvgwbruiohfgisdjklvcnkxbgvlwhreuifwhdjzlvnb jblriheifuohfiujwflksdabjkvhuwieohflqhwefknbczknjxvbiurooheqfuihejvcbad jnkvbroqhuifh jdklfjb sjdvb u4i2127 8rhe9ph fg8h434jk2n fjkfneg0 923h4thn43ljtn 234890 ht89dhklsv nfjng2pg3894ht uq2uin jefnid0 hv8792htfl 2nj 43892r fisldf ug23y3r0 hgrjl nzsfdc lmvn2983h 32h;qjrka1234u879hrgjrle gl 2jhiht43 2 32ji 1hjuo342h uth3l hjkfsdhgl'
+        }
+    ]
+
+    for test in test_vector {
+        res := base64.base64_decode(test.input) or {
+            panic("Fail")
+        }
+        assert string(res) == test.expected
+    }
+}
+
+fn test_decode_error() {
+    incorrect_string := 'abcd,,,....'
+
+    res := base64.base64_decode(incorrect_string) or {
+        return
+    }
+
+    panic("Fail: got unexpected $res")
+}
+
+fn test_roundtrip_all_bytes() {
+    mut all_possible_bytes := []byte
+    mut i := 0
+    for i < 256 {
+        all_possible_bytes << byte(i)
+        i += 1
+    }
+
+    encoded := base64.base64_encode(all_possible_bytes)
+    decoded := base64.base64_decode(encoded) or {
+        panic("Fail")
+    }
+
+    assert string(all_possible_bytes) == string(decoded)
 }
